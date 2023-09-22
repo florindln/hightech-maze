74e71b19b8c53aa9b255933037dc2cb7c6b71f24
I426239 <83040513+florindln@users.noreply.github.com>
2023-09-22 22:17:09 +0200

    add api test

--- maze solv/maze solv/maze solv/Program.cs
+++ maze solv/maze solv/maze solv/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HightechICT.Amazeing.Client.Rest;
+
 Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Add("Authorization", $"{Environment.GetEnvironmentVariable("API_TOKEN")}");
+
+
+AmazeingClient amazeingClient=new AmazeingClient("https://maze.hightechict.nl/", client);
+
+Console.WriteLine("all mazes:");
+var allMazes= await amazeingClient.AllMazes();
+
+foreach (var maze in allMazes)
+    Console.WriteLine($"[{maze.Name}] has a potential reward of [{maze.PotentialReward}] and contains [{maze.TotalTiles}] tiles;");
